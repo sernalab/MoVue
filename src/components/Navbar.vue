331ad9b5df80a7b6bb29d331b06b4bc88b46ff6b
Carles Serna Uroz <Carles.Serna.Uroz@es.ey.com>
2019-04-25 13:17:42 +0200

    navbar components created

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="navbar">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
