a9e4e5b870a8a7f85384d8c065b8bbac8f4b4e45
Carles Serna Uroz <Carles.Serna.Uroz@es.ey.com>
2019-06-03 17:00:35 +0200

    About page now is Fav page

--- src/components/Menu/Navbar.vue
+++ src/components/Menu/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <router-link to="/">Home</router-link> |
-        <router-link to="/about">Favs</router-link>
+        <router-link to="/fav">Favs</router-link>
     </div>
 </template>
 
