09cc6372ebdb955e1be92b458f5b07227ed407a8
Carles Serna Uroz <Carles.Serna.Uroz@es.ey.com>
2019-06-04 14:56:17 +0200

    ugly card

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,33 +1,21 @@
 <template>
   <div class="custom-card">
-    <div class="wrapper">
-      <div class="header">
-        <div class="date">
-          <span>12 AUG 2019</span>
-        </div>
-        <ul class="menu-content">
-          <li>
-            <a href="#" class="fa fa-bookmark-o"></a>
-          </li>
-          <li>
-            <a href="#" class="fa fa-heart-o">
-            <span>18</span></a>
-          </li>
-          <li>
-            <a href="#" class="fa fa-comment-o">
-            <span>3</span></a>
-          </li>
-        </ul>
+    <div class="header">
+      <div class="date">
+        <span>12 AUG 2019</span>
       </div>
-      <div class="data">
-        <div class="description">
-          <span class="author">Jane Doe</span>
-          <h1 class="title">
-            <a href="#">Stranger Things: The sound of the Upside Down</a>
-          </h1>
-          <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
-          <a href="#" class="button">Read more</a>
-        </div>
+      <div class="fav-icon">
+        <a href="#" class="fa fa-heart-o"></a>
+      </div>
+    </div>
+    <div class="data">
+      <div class="description">
+        <span class="author">Jane Doe</span>
+        <h1 class="title">
+          <a href="#">Stranger Things: The sound of the Upside Down</a>
+        </h1>
+        <p class="text">The antsy bingers of Netflix will eagerly anticipate the digital release of the Survive soundtrack, out today.</p>
+        <a href="#" class="button">Read more</a>
       </div>
     </div>
   </div>
