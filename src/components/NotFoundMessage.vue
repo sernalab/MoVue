2887413d98274aaf2cd231b45eac8130fe71d33a
Carles Serna Uroz <Carles.Serna.Uroz@es.ey.com>
2019-06-14 13:22:24 +0200

    added NotFound component

--- src/components/NotFoundMessage.vue
+++ src/components/NotFoundMessage.vue
@@ -1,10 +1,15 @@
 <template>
-    
+  <div class="d-flex align-items-center justify-content-center w-50 vh-100 flex-column">
+    <div class="box p-4">
+      <h1 class="cover-heading mb-3">No Movies added</h1>
+      <p class="lead">Add one by clicking in hearth icon</p>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'NotFoundMessage'
 }
 </script>
 
--- src/views/Fav.vue
+++ src/views/Fav.vue
@@ -1,21 +1,23 @@
 <template>
-  <div class="content col-12 d-flex justify-content-center flex-wrap">
+  <div class="col-12 d-flex justify-content-center flex-wrap">
     <template v-if="favourites.length">
       <Card v-for="movie in favourites" :key="movie.id" :movie="movie" />
     </template>
-    <div class="fav-not-found" v-else>
-      <p>No Movies</p>
-    </div>
+    <template v-else>
+      <NotFoundMessage />
+    </template>
   </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 import Card from '@/components/Card'
+import NotFoundMessage from '@/components/NotFoundMessage'
 
 export default {
   components: {
-    Card
+    Card,
+    NotFoundMessage
   },
   computed: {
     ...mapState(['favourites'])
